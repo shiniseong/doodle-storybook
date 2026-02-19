ce9957bc80a157f7ca6678e2ca19c135593620bc
shineseong <hss275989@gmail.com>
2026-02-20 02:40:01 +0900

    chore: Include `:server:bootstrap` module in Gradle configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 // If there are changes in only one of the projects, Gradle will rebuild only the one that has changed.
 // Learn more about structuring projects with Gradle - https://docs.gradle.org/8.7/userguide/multi_project_builds.html
 include(":shared:utils")
-include(":server:adapter:inbound", ":server:adapter:outbound", ":server:core")
+include(":server:adapter:inbound", ":server:adapter:outbound", ":server:core", ":server:bootstrap")
 include(":client")
 
 rootProject.name = "doodle-storybook"
